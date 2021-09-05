33249ffcdd66914134e9b31cd97768783f4427d9
thatshoon <gagea45@gmail.com>
2021-09-05 19:26:33 +0900

    210905

--- project/roaAuctionClawler/script.js
+++
@@ -1,6 +0,0 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const dfd = require("danfojs-node");
-const tf = require("@tensorflow/tfjs-node");
-const data = require("./data.json");
-new dfd.DataFrame(data);
